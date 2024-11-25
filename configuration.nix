430731f37c5efdbc0a33b5332c3e6d2af9475d88
l1npengtul <l1npengtul@protonmail.com>
2024-11-25 20:13:10 +0900

    Mon Nov 25 20:13:10 JST 2024 -

--- configuration.nix
+++ configuration.nix
@@ -151,6 +151,15 @@
   boot.tmp.useTmpfs = true;
   boot.tmp.cleanOnBoot = true;
   boot.loader.systemd-boot.configurationLimit = 10;
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 5d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
