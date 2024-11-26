71e354032f9f49621ac8e07f2a11f0f5ca9d6ab5
l1npengtul <l1npengtul@protonmail.com>
2024-11-26 10:06:15 +0900

    add hostname to script

--- switch.sh
+++ switch.sh
@@ -7,7 +7,7 @@ alejandra .
 
 git add .
 
-git commit  --allow-empty -m "$(date) - $@"
+git commit  --allow-empty -m "$(whoami): $(date) - $@"
 
 sudo nixos-rebuild switch --show-trace --flake .#$NIX_SWITCH_BUILD_SYSTEM_CFG_PENGPENGPENG
 
