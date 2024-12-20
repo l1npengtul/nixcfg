b1fec26aa13644ea27e44423ac0d528f8cbc5244
l1npengtul <l1npengtul@protonmail.com>
2024-12-20 14:29:04 +0900

    l1npengtul: Fri Dec 20 14:29:04 JST 2024 -

--- configuration.nix
+++ configuration.nix
@@ -1,11 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-{
-  pkgs,
-  pkgs-stable,
-  ...
-}: {
+{pkgs, ...}: {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -135,7 +131,7 @@
   ];
 
   nix.extraOptions = ''
-    trusted-users = root
+    trusted-users = root l1npengtul
   '';
 
   #   nixenvironment.pathsToLink = [ "/share/xdg-desktop-portal" "/share/applications" ];
