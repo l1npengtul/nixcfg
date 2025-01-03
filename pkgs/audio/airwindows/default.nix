5ee9e163016d662ca5ba6ba918a1e9277fb2231b
l1npengtul <l1npengtul@protonmail.com>
2025-01-04 00:53:00 +0900

    l1npengtul: Sat Jan  4 00:53:00 JST 2025 -

--- pkgs/audio/airwindows/default.nix
+++ pkgs/audio/airwindows/default.nix
@@ -19,7 +19,7 @@ stdenv.mkDerivation rec {
     owner = "baconpaul";
     repo = "airwin2rack";
     rev = "db56d13f853831ab94a5e1713282e4e518f50d5c";
-    hash = "";
+    hash = "sha256-D+Lw+3i/ME2BPwGr2S2CwonVhe+rTiVONcz4+htj+7w=";
   };
 
   nativeBuildInputs = [
