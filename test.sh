142de1d85fc5e9fbb4e44ebae3b9e3ead6ac6a58
l1npengtul <l1npengtul@protonmail.com>
2025-01-05 08:13:31 +0900

    l1npengtul: Sun Jan  5 08:13:31 JST 2025 -

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+cd ~/nixcfg
+
+alejandra .
+
+git add .
+
+git commit  --allow-empty -m "$(whoami): $(date) - $@"
+
+nixos-rebuild build --fast --show-trace --flake --verbost --L --print-build-logs .#$NIX_SWITCH_BUILD_SYSTEM_CFG_PENGPENGPENG
