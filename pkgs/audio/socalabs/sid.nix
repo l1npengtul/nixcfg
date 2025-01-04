3fb3f01de2a3faf1b916fed9972a3d4f2b3ef796
l1npengtul <l1npengtul@protonmail.com>
2025-01-04 23:28:29 +0900

    l1npengtul: Sat Jan  4 23:28:29 JST 2025 -

--- pkgs/audio/socalabs/sid.nix
+++ pkgs/audio/socalabs/sid.nix
@@ -72,6 +72,7 @@ in
 
     buildPhase = ''
       ln -s $src/CMakePresets.json /build/source/build/
+      mkdir -p /build/source/build/modules/gin/ci/
       ln -s $src/modules/gin/ci/toolchains /build/source/build/modules/gin/ci/toolchains
       ls /build/source/build/
       cmake --build --preset ninja-gcc --config Release --parallel $NIX_BUILD_CORES
