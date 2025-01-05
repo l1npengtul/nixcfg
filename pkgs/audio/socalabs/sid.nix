561d9f5ff1ae702300c62c1e58e0b58efc59470b
l1npengtul <l1npengtul@protonmail.com>
2025-01-05 10:26:39 +0900

    TEST: l1npengtul: Sun Jan  5 10:26:39 JST 2025 -

--- pkgs/audio/socalabs/sid.nix
+++ pkgs/audio/socalabs/sid.nix
@@ -25,7 +25,7 @@
       owner = "juce-framework";
       repo = "JUCE";
       rev = "5179f4e720d8406ebd1b5401c86aea8da6cc83c9";
-      hash = "";
+      hash = "sha256-faD1iI9cQ2v3YisbMDtk2lRELR7eDTz3JP0K0p1vmEU=";
     };
   };
   gin = fetchFromGitHub {
