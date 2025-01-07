846d550d4e9ff2b8115f8aa2ab87310add38b8c7
l1npengtul <l1npengtul@protonmail.com>
2025-01-08 02:57:54 +0900

    l1npengtul: Wed Jan  8 02:57:54 JST 2025 -

--- pkgs/audio/default.nix
+++ pkgs/audio/default.nix
@@ -14,33 +14,7 @@
   sn76489 = pkgs.callPackage ./socalabs/sn76489.nix {};
   voc = pkgs.callPackage ./socalabs/voc.nix {};
   wavetable = pkgs.callPackage ./socalabs/wavetable.nix {};
-  wsjtz = {
-    wsjtx,
-    fetchzip,
-    lib,
-  }:
-    wsjtx.overrideAttrs (old: rec {
-      pname = "wsjtz";
-      version = "2.7.0-rc7-1.43";
-
-      src = fetchzip {
-        url = "https://psychz.dl.sourceforge.net/project/wsjt-z/Source/wsjtz-${version}.zip";
-        hash = "sha256-m+P83S5P9v3NPtifc+XjZm/mAOs+NT9fTWXisxuWtZo=";
-      };
-
-      postFixup = ''
-        mv $out/bin/wsjtx $out/bin/wsjtz
-        mv $out/bin/wsjtx_app_version $out/bin/wsjtz_app_version
-      '';
-
-      meta = with lib; {
-        description = "WSJT-X fork, primarily focused on automation and enhanced functionality";
-        homepage = "https://sourceforge.net/projects/wsjt-z/";
-        license = lib.licenses.gpl3;
-        platforms = platforms.linux;
-        mainProgram = "wsjtz";
-      };
-    });
+  wsjtz = pkgs.callPackage ./test.nix {};
   airwindows = pkgs.callPackage ./airwindows {};
   #   synthv-studio-pro = pkgs.callPackage ./synthv-studio-pro {};
   recstar = pkgs.callPackage ./recstar {};
