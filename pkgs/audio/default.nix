33644912eae37fe5a8651f1e9b9e65d49983417e
l1npengtul <l1npengtul@protonmail.com>
2025-01-09 01:50:53 +0900

    l1npengtul: Thu Jan  9 01:50:53 JST 2025 -

--- pkgs/audio/default.nix
+++ pkgs/audio/default.nix
@@ -49,8 +49,8 @@ in {
     distrho-ports
     airwindows-lv2
     pkgs-stable.bitwig-studio
-    pkgs-stable.yabridgectl
-    pkgs-stable.yabridge
+    yabridgectl
+    yabridge
     pkgs-stable.wineWowPackages.stagingFull
     dxvk_2
     recstar
