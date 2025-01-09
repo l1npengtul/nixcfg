1be5cec14a9c393bc69f03076904b646b2fade98
l1npengtul <l1npengtul@protonmail.com>
2025-01-10 02:20:41 +0900

    l1npengtul: Fri Jan 10 02:20:41 JST 2025 -

--- pkgs/audio/socalabs/sn76489.nix
+++ pkgs/audio/socalabs/sn76489.nix
@@ -155,7 +155,7 @@ in
 
       install -Dm755 SN76489_artefacts/Release/Standalone/SN76489 $out/bin
 
-      install -Dm444 $src/plugin/Resources/icon.png $out/share/pixmaps/SN76489.png
+      install -Dm444 $src/plugin/Resources/logo.png $out/share/pixmaps/SN76489.png
 
       runHook postInstall
     '';
