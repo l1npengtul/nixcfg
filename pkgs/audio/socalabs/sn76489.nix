90721ea1865c3f9267598067c11f000ea81855c1
l1npengtul <l1npengtul@protonmail.com>
2025-01-10 01:42:34 +0900

    l1npengtul: Fri Jan 10 01:42:34 JST 2025 -

--- pkgs/audio/socalabs/sn76489.nix
+++ pkgs/audio/socalabs/sn76489.nix
@@ -4,103 +4,163 @@
   lib,
   cmake,
   pkg-config,
-  gcc12,
   alsa-lib,
+  copyDesktopItems,
+  makeDesktopItem,
   xorg,
   freetype,
-  libGLU,
+  expat,
+  libGL,
   libjack2,
-  ninja,
-  ladspa-sdk,
   curl,
-  mesa,
-  webkitgtk,
-  juce,
-}: let
-  plname = "SN76489";
-in
-  stdenv.mkDerivation rec {
-    pname = "socalabs-sn76489";
-    version = "1.1.0";
+  webkitgtk_4_0,
+  libsysprof-capture,
+  pcre2,
+  util-linux,
+  libselinux,
+  libsepol,
+  libthai,
+  libxkbcommon,
+  libdatrie,
+  libepoxy,
+  libsoup_2_4,
+  lerc,
+  sqlite,
+  # Disable VST building by default, since NixOS doesn't have a VST license
+  enableVST2 ? false,
+}:
+stdenv.mkDerivation {
+  pname = "socalabs-SN76489";
+  version = "1.1.0";
 
-    src =
-      (fetchFromGitHub
-        {
-          owner = "FigBug";
-          repo = plname;
-          rev = "210e0c799727be07cb7f9559af40616e17f20302";
-          hash = "";
-          fetchSubmodules = true;
-        })
-      .overrideAttrs (_: {
-        GIT_CONFIG_COUNT = 1;
-        GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
-        GIT_CONFIG_VALUE_0 = "git@github.com:";
-      });
+  src =
+    (fetchFromGitHub {
+      owner = "FigBug";
+      repo = "SN76489";
+      rev = "210e0c799727be07cb7f9559af40616e17f20302";
+      hash = "";
+      fetchSubmodules = true;
+    })
+    .overrideAttrs
+    (_: {
+      GIT_CONFIG_COUNT = 1;
+      GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+      GIT_CONFIG_VALUE_0 = "git@github.com:";
+    });
 
-    nativeBuildInputs = [
-      cmake
-      pkg-config
-      ninja
-      juce
-      gcc12
-    ];
+  desktopItems = [
+    (makeDesktopItem {
+      type = "Application";
+      name = "SN76489";
+      desktopName = "Socalabs SN76489";
+      comment = "Socalabs Commodore 64 SN76489 Emulation Plugin (Standalone)";
+      icon = "SN76489";
+      exec = "SN76489";
+      categories = [
+        "Audio"
+        "AudioVideo"
+      ];
+    })
+  ];
 
-    buildInputs = [
-      gcc12
-      alsa-lib
-      xorg.libX11
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXinerama
-      xorg.libXrandr
-      xorg.xvfb
-      libGLU
-      libjack2
-      freetype
-      ladspa-sdk
-      curl
-      mesa
-      webkitgtk
-    ];
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    copyDesktopItems
+  ];
 
-    cmakeFlags = [
-      "--preset ninja-gcc"
-    ];
+  buildInputs = [
+    alsa-lib
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXinerama
+    xorg.libXrandr
+    xorg.libXtst
+    xorg.libXdmcp
+    libGL
+    libjack2
+    libsysprof-capture
+    libselinux
+    libsepol
+    libthai
+    libxkbcommon
+    libdatrie
+    libepoxy
+    libsoup_2_4
+    lerc
+    freetype
+    curl
+    webkitgtk_4_0
+    pcre2
+    util-linux
+    sqlite
+    expat
+  ];
 
-    cmakeBuildType = "Release";
+  cmakeFlags = [
+    (lib.cmakeBool "JUCE_COPY_PLUGIN_AFTER_BUILD" false)
+    "-DCMAKE_AR=${stdenv.cc.cc}/bin/gcc-ar"
+    "-DCMAKE_RANLIB=${stdenv.cc.cc}/bin/gcc-ranlib"
+    "-DCMAKE_NM=${stdenv.cc.cc}/bin/gcc-nm"
+  ];
 
-    buildPhase = ''
-      cmake --build --preset ninja-gcc --config Release --parallel $NIX_BUILD_CORES
-    '';
+  # enable LTO flags. disable at your peril! (too long didnt run - makes the linking process take 10 years)
 
-    installPhase = ''
-      runHook preInstall
+  patchPhase = ''
+    sed -i '159i juce::juce_recommended_lto_flags' CMakeLists.txt
+    substituteInPlace CMakeLists.txt \
+    --replace-fail 'FORMATS Standalone VST VST3 AU LV2' 'FORMATS Standalone ${lib.optionalString enableVST2 "VST"} VST3 LV2'
+  '';
 
-      mkdir -p $out/lib/vst3 $out/lib/vst $out/lib/lv2
+  cmakeBuildType = "Release";
 
-        cp -R Builds/ninja-gcc/${plname}_artefacts/Release/LV2/${plname}.lv2 $out/lib/lv2
-        cp -R Builds/ninja-gcc/${plname}_artefacts/Release/VST/lib${plname}.so $out/lib/vst
-        cp -R Builds/ninja-gcc/${plname}_artefacts/Release/VST3/${plname}.vst3 $out/lib/vst3
+  strictDeps = true;
 
-      runHook postInstall
-    '';
+  preBuild = ''
+    export HOME=$(pwd)/home
+    mkdir -p $HOME
+  '';
 
-    NIX_LDFLAGS = (
-      toString [
-        "-lX11"
-        "-lXcomposite"
-        "-lXcursor"
-        "-lXinerama"
-        "-lXrandr"
-      ]
-    );
+  installPhase = ''
+    runHook preInstall
 
-    meta = {
-      description = "Socalabs Texas Instruments SN76489 from Sega Master System Emulation Plugin";
-      homepage = "https://socalabs.com/synths/sn76489/";
-      platforms = ["x86_64-linux"];
-      license = lib.licenses.gpl3;
-      maintainers = with lib.maintainers; [l1npengtul];
-    };
-  }
+    mkdir -p $out/lib/vst3 $out/lib/lv2 $out/bin
+
+    ${lib.optionalString enableVST2 ''
+      mkdir -p $out/lib/vst
+      cp -r SN76489_artefacts/Release/VST/libSN76489.so $out/lib/vst
+    ''}
+
+    cp -r SN76489_artefacts/Release/LV2/SN76489.lv2 $out/lib/lv2
+    cp -r SN76489_artefacts/Release/VST3/SN76489.vst3 $out/lib/vst3
+
+    install -Dm755 SN76489_artefacts/Release/Standalone/SN76489 $out/bin
+
+    install -Dm444 $src/plugin/Resources/icon.png $out/share/pixmaps/SN76489.png
+
+    runHook postInstall
+  '';
+
+  NIX_LDFLAGS = (
+    toString [
+      "-lX11"
+      "-lXext"
+      "-lXcomposite"
+      "-lXcursor"
+      "-lXinerama"
+      "-lXrandr"
+      "-lXtst"
+      "-lXdmcp"
+    ]
+  );
+
+  meta = {
+    description = "Socalabs Commodore 64 SN76489 Emulation Plugin";
+    homepage = "https://socalabs.com/synths/commodore-64-sid/";
+    platforms = lib.platforms.linux;
+    mainProgram = "SN76489";
+    license = [lib.licenses.gpl3] ++ lib.optional enableVST2 lib.licenses.unfree;
+    maintainers = with lib.maintainers; [l1npengtul];
+  };
+}
