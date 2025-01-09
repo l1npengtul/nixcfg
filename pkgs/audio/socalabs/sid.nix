22c387a38101816b88c8d9726e7332838bbdefc3
l1npengtul <l1npengtul@protonmail.com>
2025-01-10 05:31:00 +0900

    l1npengtul: Fri Jan 10 05:31:00 JST 2025 -

--- pkgs/audio/socalabs/sid.nix
+++ pkgs/audio/socalabs/sid.nix
@@ -26,169 +26,153 @@
   libsoup_2_4,
   lerc,
   sqlite,
-  juce,
-  srcOnly,
   ninja,
   # Disable VST building by default, since NixOS doesn't have a VST license
   enableVST2 ? false,
-}: let
-  gin = fetchFromGitHub {
-    owner = "FigBug";
-    repo = "gin";
-    rev = "f5c6bbcc82fd63ebc5c08f2edb20f5f3a0ba0128";
-    hash = "sha256-YX/DiTWBrcn57hR1vPNajQ3TeXzvBtH622xbNN1x00U=";
+}:
+stdenv.mkDerivation {
+  pname = "socalabs-sid";
+  version = "1.1.0";
+
+  src =
+    (fetchFromGitHub {
+      owner = "FigBug";
+      repo = "SID";
+      rev = "bb826fdea39da0804c53d81d35bea29aeff4436d";
+      hash = "sha256-6IStysItOS7EltTCqdyo9vrsnSA1YYoN4y8Bjv1fhNk=";
+      fetchSubmodules = true;
+    })
+    .overrideAttrs
+    (_: {
+      GIT_CONFIG_COUNT = 1;
+      GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
+      GIT_CONFIG_VALUE_0 = "git@github.com:";
+    });
+
+  desktopItems = [
+    (makeDesktopItem {
+      type = "Application";
+      name = "socalabs-sid";
+      desktopName = "Socalabs SID";
+      comment = "Socalabs Commodore 64 SID Emulation Plugin (Standalone)";
+      icon = "SID";
+      exec = "SID";
+      categories = [
+        "Audio"
+        "AudioVideo"
+      ];
+    })
+  ];
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    copyDesktopItems
+    ninja
+  ];
+
+  buildInputs = [
+    alsa-lib
+    xorg.libX11
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libXinerama
+    xorg.libXrandr
+    xorg.libXtst
+    xorg.libXdmcp
+    xorg.xvfb
+    libGL
+    libjack2
+    libsysprof-capture
+    libselinux
+    libsepol
+    libthai
+    libxkbcommon
+    libdatrie
+    libepoxy
+    libsoup_2_4
+    lerc
+    freetype
+    curl
+    webkitgtk_4_0
+    pcre2
+    util-linux
+    sqlite
+    expat
+  ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "JUCE_COPY_PLUGIN_AFTER_BUILD" false)
+    "-DCMAKE_AR=${stdenv.cc.cc}/bin/gcc-ar"
+    "-DCMAKE_RANLIB=${stdenv.cc.cc}/bin/gcc-ranlib"
+    "-DCMAKE_NM=${stdenv.cc.cc}/bin/gcc-nm"
+    "--preset ninja-gcc"
+  ];
+
+  # enable LTO flags. disable at your peril! (too long didnt run - makes the linking process take 10 years)
+
+  patchPhase = ''
+    sed -i '159i juce::juce_recommended_lto_flags' CMakeLists.txt
+    substituteInPlace CMakeLists.txt \
+    --replace-fail 'FORMATS Standalone VST VST3 AU LV2' 'FORMATS Standalone ${lib.optionalString enableVST2 "VST"} VST3 LV2'
+
+    # we need to patch JUCE itself to enable jack MIDI support
+    # please https://github.com/juce-framework/JUCE/issues/952
+    # TODO: remove when juce updates :D
+    substituteInPlace modules/juce/modules/juce_audio_devices/native/juce_Midi_linux.cpp \
+    --replace-fail "port = client.createPort (portName, forInput, false);" "port = client.createPort (portName, forInput, true);"
+  '';
+
+  cmakeBuildType = "Release";
+
+  strictDeps = true;
+
+  preBuild = ''
+    export HOME=$(pwd)/home
+    mkdir -p $HOME
+
+    cd ../Builds/ninja-gcc
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib/vst3 $out/lib/lv2 $out/bin
+
+    ${lib.optionalString enableVST2 ''
+      mkdir -p $out/lib/vst
+      cp -r SID_artefacts/Release/VST/libSID.so $out/lib/vst
+    ''}
+
+    cp -r SID_artefacts/Release/LV2/SID.lv2 $out/lib/lv2
+    cp -r SID_artefacts/Release/VST3/SID.vst3 $out/lib/vst3
+
+    install -Dm755 SID_artefacts/Release/Standalone/SID $out/bin
+
+    install -Dm444 $src/plugin/Resources/icon.png $out/share/pixmaps/SID.png
+
+    runHook postInstall
+  '';
+
+  NIX_LDFLAGS = (
+    toString [
+      "-lX11"
+      "-lXext"
+      "-lXcomposite"
+      "-lXcursor"
+      "-lXinerama"
+      "-lXrandr"
+      "-lXtst"
+      "-lXdmcp"
+    ]
+  );
+
+  meta = {
+    description = "Socalabs Commodore 64 SID Emulation Plugin";
+    homepage = "https://socalabs.com/synths/commodore-64-sid/";
+    platforms = lib.platforms.linux;
+    mainProgram = "SID";
+    license = [lib.licenses.gpl3] ++ lib.optional enableVST2 lib.licenses.unfree;
+    maintainers = with lib.maintainers; [l1npengtul];
   };
-in
-  stdenv.mkDerivation {
-    pname = "socalabs-sid";
-    version = "1.1.0";
-
-    dontStrip = true;
-    dontPatchELF = true;
-
-    src =
-      (fetchFromGitHub {
-        owner = "FigBug";
-        repo = "SID";
-        rev = "bb826fdea39da0804c53d81d35bea29aeff4436d";
-        hash = "sha256-6IStysItOS7EltTCqdyo9vrsnSA1YYoN4y8Bjv1fhNk=";
-        fetchSubmodules = true;
-      })
-      .overrideAttrs
-      (_: {
-        GIT_CONFIG_COUNT = 1;
-        GIT_CONFIG_KEY_0 = "url.https://github.com/.insteadOf";
-        GIT_CONFIG_VALUE_0 = "git@github.com:";
-      });
-
-    desktopItems = [
-      (makeDesktopItem {
-        type = "Application";
-        name = "socalabs-sid";
-        desktopName = "Socalabs SID";
-        comment = "Socalabs Commodore 64 SID Emulation Plugin (Standalone)";
-        icon = "SID";
-        exec = "SID";
-        categories = [
-          "Audio"
-          "AudioVideo"
-        ];
-      })
-    ];
-
-    nativeBuildInputs = [
-      cmake
-      pkg-config
-      copyDesktopItems
-      ninja
-    ];
-
-    buildInputs = [
-      alsa-lib
-      xorg.libX11
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXinerama
-      xorg.libXrandr
-      xorg.libXtst
-      xorg.libXdmcp
-      xorg.xvfb
-      libGL
-      libjack2
-      libsysprof-capture
-      libselinux
-      libsepol
-      libthai
-      libxkbcommon
-      libdatrie
-      libepoxy
-      libsoup_2_4
-      lerc
-      freetype
-      curl
-      webkitgtk_4_0
-      pcre2
-      util-linux
-      sqlite
-      expat
-      juce
-    ];
-
-    cmakeFlags = [
-      (lib.cmakeBool "JUCE_COPY_PLUGIN_AFTER_BUILD" false)
-      "-DCMAKE_AR=${stdenv.cc.cc}/bin/gcc-ar"
-      "-DCMAKE_RANLIB=${stdenv.cc.cc}/bin/gcc-ranlib"
-      "-DCMAKE_NM=${stdenv.cc.cc}/bin/gcc-nm"
-      "--preset ninja-gcc"
-    ];
-
-    # enable LTO flags. disable at your peril! (too long didnt run - makes the linking process take 10 years)
-
-    patchPhase = ''
-      sed -i '159i juce::juce_recommended_lto_flags' CMakeLists.txt
-      substituteInPlace CMakeLists.txt \
-      --replace-fail 'FORMATS Standalone VST VST3 AU LV2' 'FORMATS Standalone ${lib.optionalString enableVST2 "VST"} VST3 LV2'
-      #substituteInPlace plugin/Source/PluginProcessor.h \
-      #--replace-fail 'class SIDAudioProcessor : public gin::Processor' 'class SIDAudioProcessor : public gin::Processor, public gin::Synthesiser'
-
-      # patch gin to latest
-      #rm -rf modules/gin
-      #ln -s ${gin} modules/gin
-      # patch JUCE to latest
-      #rm -rf modules/juce
-      #ln -s ${srcOnly juce} modules/juce
-    '';
-
-    cmakeBuildType = "Release";
-
-    strictDeps = true;
-
-    preBuild = ''
-      export HOME=$(pwd)/home
-      mkdir -p $HOME
-
-      cd ../Builds/ninja-gcc
-    '';
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/lib/vst3 $out/lib/lv2 $out/bin
-
-      ${lib.optionalString enableVST2 ''
-        mkdir -p $out/lib/vst
-        cp -r SID_artefacts/Release/VST/libSID.so $out/lib/vst
-      ''}
-
-      cp -r SID_artefacts/Release/LV2/SID.lv2 $out/lib/lv2
-      cp -r SID_artefacts/Release/VST3/SID.vst3 $out/lib/vst3
-
-      install -Dm755 SID_artefacts/Release/Standalone/SID $out/bin
-
-      install -Dm444 $src/plugin/Resources/icon.png $out/share/pixmaps/SID.png
-
-      runHook postInstall
-    '';
-
-    NIX_LDFLAGS = (
-      toString [
-        "-lX11"
-        "-lXext"
-        "-lXcomposite"
-        "-lXcursor"
-        "-lXinerama"
-        "-lXrandr"
-        "-lXtst"
-        "-lXdmcp"
-      ]
-    );
-
-    meta = {
-      description = "Socalabs Commodore 64 SID Emulation Plugin";
-      homepage = "https://socalabs.com/synths/commodore-64-sid/";
-      platforms = lib.platforms.linux;
-      mainProgram = "SID";
-      license = [lib.licenses.gpl3] ++ lib.optional enableVST2 lib.licenses.unfree;
-      maintainers = with lib.maintainers; [l1npengtul];
-    };
-  }
+}
