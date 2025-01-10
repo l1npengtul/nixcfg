2ceef22cad572bc5f477c5b28df3612acb0f4387
l1npengtul <l1npengtul@protonmail.com>
2025-01-11 00:53:24 +0900

    l1npengtul: Sat Jan 11 00:53:24 JST 2025 -

--- pkgs/audio/airwindows/individual.nix
+++ pkgs/audio/airwindows/individual.nix
@@ -6,43 +6,24 @@
   cmake,
   pkg-config,
 }: let
-  # the project expects a CMakeLists.txt to exist within the VSTSDK directory, but ours doesn't.cmakelist
-  # Instead, we make our own.
   # adapted from oxefmsynth
-  vst-sdk =
-    #     let
-    #       cmakelist = builtins.toFile "CMakeLists.txt" ''
-    #         cmake_minimum_required(VERSION 3.9)
-    #
-    #         project (VST_SDK)
-    #
-    #         aux_source_directory(''${CMAKE_CURRENT_SOURCE_DIR}/ SOURCE_LIB)
-    #
-    #         add_library(VST_SDK STATIC ''${SOURCE_LIB})
-    #         target_include_directories(VST_SDK PUBLIC ''${CMAKE_CURRENT_SOURCE_DIR}/ ''${CMAKE_CURRENT_SOURCE_DIR}/pluginterfaces ''${CMAKE_CURRENT_SOURCE_DIR}/pluginterfaces/vst2.x)
-    #         set_property(TARGET VST_SDK PROPERTY POSITION_INDEPENDENT_CODE ON)
-    #       '';
-    #
-    #     in
-    stdenv.mkDerivation {
-      dontConfigure = true;
-      dontPatch = true;
-      dontBuild = true;
-      dontStrip = true;
-      dontPatchELF = true;
+  vst-sdk = stdenv.mkDerivation {
+    dontConfigure = true;
+    dontPatch = true;
+    dontBuild = true;
+    dontStrip = true;
+    dontPatchELF = true;
 
-      name = "vstsdk3610_11_06_2018_build_37";
-      src = fetchzip {
-        url = "https://web.archive.org/web/20181016150224if_/https://download.steinberg.net/sdk_downloads/vstsdk3610_11_06_2018_build_37.zip";
-        sha256 = "0da16iwac590wphz2sm5afrfj42jrsnkr1bxcy93lj7a369ildkj";
-      };
-
-      installPhase = ''
-        ls
-        cp -r VST2_SDK $out
-        ls $out
-      '';
+    name = "vstsdk3610_11_06_2018_build_37";
+    src = fetchzip {
+      url = "https://web.archive.org/web/20181016150224if_/https://download.steinberg.net/sdk_downloads/vstsdk3610_11_06_2018_build_37.zip";
+      sha256 = "0da16iwac590wphz2sm5afrfj42jrsnkr1bxcy93lj7a369ildkj";
     };
+
+    installPhase = ''
+      cp -r VST2_SDK $out
+    '';
+  };
 in
   stdenv.mkDerivation {
     pname = "airwindows";
@@ -60,8 +41,6 @@ in
     prePatch = ''
       mkdir -p plugins/LinuxVST/include
       ln -s ${vst-sdk.out} plugins/LinuxVST/include/vstsdk
-      ls -l plugins/LinuxVST/include/vstsdk
-      ls -l ${vst-sdk.out}
     '';
 
     patches = [
