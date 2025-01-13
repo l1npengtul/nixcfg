aca5fc6e5056b0c2465e56cf9326e9599094fdfa
l1npengtul <l1npengtul@protonmail.com>
2025-01-14 08:04:32 +0900

    l1npengtul: Tue Jan 14 08:04:32 JST 2025 -

--- pkgs/audio/default.nix
+++ pkgs/audio/default.nix
@@ -44,7 +44,7 @@ in {
     chow-tape-model
     vcv-rack
     cardinal
-    pkgs-master.carla
+    carla
     alsa-utils
     vital
     distrho-ports
@@ -58,12 +58,12 @@ in {
     dxvk_2
     recstar
 
-    airwindows-ind
-    sid
-    sn76489
-    papu
-    rp2a03
-    voc
+    #airwindows-ind
+    #sid
+    #sn76489
+    #papu
+    #rp2a03
+    #voc
     #slplugins
 
     inputs.audio.packages.${pkgs.system}.paulxstretch
