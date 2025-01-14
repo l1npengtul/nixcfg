86796415c77ca578fe312b71fb2a763f3252224f
l1npengtul <l1npengtul@protonmail.com>
2025-01-14 20:09:00 +0900

    l1npengtul@oldhome: Tue Jan 14 20:09:00 JST 2025 -

--- pkgs/audio/audiothing/minibit.nix
+++ pkgs/audio/audiothing/minibit.nix
@@ -93,10 +93,9 @@ in
     postFixup = ''
       find $out -type f -executable | while IFS= read -r f ; do
         wrapProgram $f \
-          "''${gappsWrapperArgs[@]}" \
+          --run "${setup}" \
           --suffix PATH : "${lib.makeBinPath [xdg-utils]}" \
-          --suffix LD_LIBRARY_PATH : "${lib.strings.makeLibraryPath buildInputs}" \
-          --run "${setup}"
+          --suffix LD_LIBRARY_PATH : "${lib.strings.makeLibraryPath buildInputs}"
       done
     '';
 
