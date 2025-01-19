0c347cbd1e9b6a3fdf78ae594c6b20b959ceeb3f
l1npengtul <l1npengtul@protonmail.com>
2025-01-20 08:52:41 +0900

    waw

---
+++ hosts/common/remote.nix
@@ -0,0 +1,15 @@
+{}: {
+  users.users.remotebuild = {
+    isNormalUser = true;
+    createHome = false;
+    group = "remotebuild";
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6Eat2YPbwb1FMFXXr2JWVY75l5hba3GFBuQMh/y2N/ root@oldhome"
+    ];
+  };
+
+  users.groups.remotebuild = {};
+
+  nix.settings.trusted-users = ["remotebuild"];
+}
