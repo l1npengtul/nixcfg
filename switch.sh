285c833229dfd7d7f97a66ec732b2d7479560ec2
l1npengtul <l1npengtul@protonmail.com>
2025-01-20 13:52:37 +0900

    l1npengtul@oldhome: Mon Jan 20 13:52:37 JST 2025 -

--- switch.sh
+++ switch.sh
@@ -11,7 +11,7 @@ git add .
 
 git commit  --allow-empty -m "$(whoami)@${WHERE}: $(date) - $@"
 
-sudo nixos-rebuild switch --show-trace --flake .#$WHERE
+sudo nixos-rebuild switch --show-trace --builders "" --flake .#$WHERE
 
 nix-env --delete-generations 7d
 
