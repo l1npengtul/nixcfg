095608ac84b18645532f35f9c3774c335efe5e6e
l1npengtul <l1npengtul@protonmail.com>
2025-01-20 15:13:57 +0900

    l1npengtul@oldhome: Mon Jan 20 15:13:57 JST 2025

--- pkgs/audio/default.nix
+++ pkgs/audio/default.nix
@@ -58,12 +58,12 @@ in {
     dxvk_2
     recstar
 
-    #airwindows-ind
+    airwindows-ind
     sid
-    #sn76489
-    #papu
-    #rp2a03
-    #voc
+    sn76489
+    papu
+    rp2a03
+    voc
     #slplugins
 
     inputs.audio.packages.${pkgs.system}.paulxstretch
