f21e24e69a2c9b3441419ec03674f417c79f347c
l1npengtul <l1npengtul@protonmail.com>
2025-01-25 15:32:14 +0900

    l1npengtul@oldhome: Sat Jan 25 15:32:14 JST 2025

--- pkgs/audio/socalabs/organ.nix
+++ pkgs/audio/socalabs/organ.nix
@@ -40,7 +40,7 @@ stdenv.mkDerivation {
       owner = "FigBug";
       repo = "Organ";
       rev = "17f884126bb76d2d3c95e12224aab8f38ef8c96d";
-      hash = "";
+      hash = "sha256-gpC1+TfzLojJuE24aQUrOl3HIzC7x3CMrB4h7GWfZv0=";
       fetchSubmodules = true;
     })
     .overrideAttrs
