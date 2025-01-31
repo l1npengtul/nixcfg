2a7721d2811ec4563c51acb2da2d7327e6ec4742
l1npengtul <l1npengtul@protonmail.com>
2025-01-31 17:11:31 +0900

    l1npengtul@oldhome: Fri Jan 31 17:11:31 JST 2025 -

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -14,10 +14,13 @@
 
   programs.nix-ld = {
     enable = true;
-    libraries = [pkgs.steam-run.fhsenv.args.multiPkgs pkgs];
+    libraries = [
+      (pkgs.runCommand "steamrun-lib" {}
+        "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
+    ];
   };
 
-  environment.systemPackages = with pkgs; [protonup-qt];
+  environment.systemPackages = with pkgs; [protonup-qt nix-alien];
 
   hardware.steam-hardware.enable = true;
 }
