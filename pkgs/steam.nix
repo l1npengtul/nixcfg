e7402294446b0b16eacb4a81618430737d2282f5
l1npengtul <l1npengtul@protonmail.com>
2025-01-31 17:05:35 +0900

    l1npengtul@oldhome: Fri Jan 31 17:05:35 JST 2025 -

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -1,14 +1,10 @@
 {pkgs, ...}: {
-  programs.java.enable = true;
-
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
     gamescopeSession.enable = true;
-
-    package = pkgs.steam.override {withJava = true;};
   };
 
   programs.gamescope = {
