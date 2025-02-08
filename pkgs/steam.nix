2fa894748aad519198137d8e3c8bf16bf88ff3fb
l1npengtul <l1npengtul@protonmail.com>
2025-02-09 00:56:54 +0900

    l1npengtul@oldhome: Sun Feb  9 00:56:54 JST 2025 -

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -20,7 +20,7 @@
     ];
   };
 
-  environment.systemPackages = with pkgs; [protonup-qt];
+  environment.systemPackages = with pkgs; [protonup-qt r2modman];
 
   hardware.steam-hardware.enable = true;
 }
