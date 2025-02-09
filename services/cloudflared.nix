747e7606956730a91f234e90f748bb9f959de2c0
l1npengtul <l1npengtul@protonmail.com>
2025-02-10 08:41:24 +0900

    pengsrv@abandonedfactory: Mon Feb 10 08:41:24 AM JST 2025 -

--- services/cloudflared.nix
+++ services/cloudflared.nix
@@ -7,7 +7,7 @@
   environment.systemPackages = [pkgs.cloudflared];
 
   services.cloudflared = {
-    enabled = true;
+    enable = true;
     tunnels = {
       "c0a14307-5aae-475e-830b-e7fcc62a567d" = {
         credintalsFile = "${config.age.secrets.cloudflared-minecraftproxy-secret.path}";
