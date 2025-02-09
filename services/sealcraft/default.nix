0f750b5af4d7915568b73243403f8f36fa6364f7
l1npengtul <l1npengtul@protonmail.com>
2025-02-09 18:20:20 +0900

    wadwa

--- services/sealcraft/default.nix
+++ services/sealcraft/default.nix
@@ -1,1 +1,19 @@
-{}: {}
+{pkgs-stable, ...}:
+
+{
+
+    flux = {
+        enable = true;
+        servers = {
+            sealcraft = {
+                package = pkgs-stable.mkMinecraftServer {
+                    name = "sealcraft";
+                    src = ./sealcraft_config;
+                    hash = "";
+                };
+                proxy.enable = true;
+                proxy.backend = "playit";
+            };
+        };
+    };
+}
