217f52f14c72cbf2ab342e13bcbb6af51231a5ac
l1npengtul <l1npengtul@protonmail.com>
2025-02-19 13:34:16 +0900

    l1npengtul@pegrose512: Wed Feb 19 13:34:16 KST 2025 -

--- plasma/pegrose512.nix
+++ plasma/pegrose512.nix
@@ -444,7 +444,6 @@
       "kwinrc"."TouchEdges"."Top" = "KRunner";
       "kwinrc"."Wayland"."InputMethod[$e]" = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
       "kwinrc"."Wayland"."InputMethod\x5b$e\x5d" = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
-      "kwinrc"."Wayland"."InputMethodx5b$ex5d" = "/run/current-system/sw/share/applications/fcitx5-wayland-launcher.desktop";
       "kwinrc"."Wayland"."VirtualKeyboardEnabled" = true;
       "kwinrc"."Windows"."FocusPolicy" = "FocusFollowsMouse";
       "kwinrc"."Windows"."FocusStealingPreventionLevel" = 2;
