fd769b56e480f27e1da4cfabe4cbcd0fcfe79060
l1npengtul <l1npengtul@protonmail.com>
2025-02-23 14:14:04 +0900

    pengsrv@abandonedfactory: Sun Feb 23 02:14:04 PM JST 2025 -

--- hosts/s-23sierpinski/default.nix
+++ hosts/s-23sierpinski/default.nix
@@ -1,9 +1,6 @@
-{
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
-#     ./hardware-configuration.nix
+    #     ./hardware-configuration.nix
     ./../common/use-remote.nix
   ];
   environment.systemPackages = with pkgs; [
