72ba52f2d01ec3810e1da2f4d98ee330d7ebc158
l1npengtul <l1npengtul@protonmail.com>
2025-02-23 14:29:17 +0900

    l1npengtul@pegrose512: Sun Feb 23 14:29:17 KST 2025 -

--- applications/individual/vscode.nix
+++ applications/individual/vscode.nix
@@ -2,7 +2,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       zhuangtongfa.material-theme
       yzhang.markdown-all-in-one
       mkhl.direnv
