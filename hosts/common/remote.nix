0d436949f1d8e596be3ede459d7b4e1f8754d2a6
l1npengtul <l1npengtul@protonmail.com>
2025-02-23 15:30:09 +0900

    l1npengtul@s-23sierpinski: Sun Feb 23 03:30:09 PM JST 2025 -

--- hosts/common/remote.nix
+++ hosts/common/remote.nix
@@ -6,6 +6,7 @@
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH6Eat2YPbwb1FMFXXr2JWVY75l5hba3GFBuQMh/y2N/ root@oldhome"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGj9wCUYzGaf1gClP9YbJNMxxTVOPS2tyFP870bcgZEH root@s-23sierpinski"
     ];
   };
 
