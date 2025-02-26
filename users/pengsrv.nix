59c2ac44e115410b3a7e5db37474f9dacf302b59
l1npengtul <l1npengtul@protonmail.com>
2025-02-26 10:36:28 +0900

    l1npengtul@pegrose512: Wed Feb 26 10:36:28 KST 2025 -

--- users/pengsrv.nix
+++ users/pengsrv.nix
@@ -1,83 +1,8 @@
 {pkgs, ...}: {
   imports = [
     ./common.nix
+    ./common-plasma.nix
   ];
-
-  programs = {
-    git = {
-      enable = true;
-      userEmail = "l1npengtul@protonmail.com";
-      userName = "l1npengtul";
-    };
-
-    carapace.enable = true;
-    carapace.enableNushellIntegration = true;
-
-    starship = {
-      enable = true;
-      settings = {
-        add_newline = true;
-        character = {
-          success_symbol = "[➜](bold green)";
-          error_symbol = "[➜](bold red)";
-        };
-      };
-    };
-  };
-
-  programs.nushell = {
-    enable = true;
-    # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
-    configFile.source = ./config.nu;
-    # for editing directly to config.nu
-    extraConfig = ''
-      let carapace_completer = {|spans|
-      carapace $spans.0 nushell $spans | from json
-      }
-      $env.config = {
-        hooks: {
-          pre_prompt: [{ ||
-            if (which direnv | is-empty) {
-              return
-            }
-
-            direnv export json | from json | default {} | load-env
-          }]
-        }
-       show_banner: false,
-       completions: {
-       case_sensitive: false # case-sensitive completions
-       quick: true    # set to false to prevent auto-selecting completions
-       partial: true    # set to false to prevent partial filling of the prompt
-       algorithm: "fuzzy"    # prefix or fuzzy
-       external: {
-       # set to false to prevent nushell looking into $env.PATH to find more suggestions
-           enable: true
-       # set to lower can improve completion performance at the cost of omitting some options
-           max_results: 100
-           completer: $carapace_completer # check 'carapace_completer'
-         }
-       }
-      }
-      $env.PATH = ($env.PATH |
-      split row (char esep) |
-      prepend /home/myuser/.apps |
-      append /usr/bin/env
-      )
-    '';
-  };
-
-  xdg.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-kde];
-  xdg.portal.config.common.default = "*";
-
-  dconf.settings = {
-    "org/virt-manager/virt-manager/connections" = {
-      autoconnect = ["qemu:///system"];
-      uris = ["qemu:///system"];
-    };
-  };
 
   home = {
     packages = with pkgs; [
@@ -85,7 +10,5 @@
       xdg-desktop-portal-kde
       devenv
     ];
-
-    stateVersion = "24.05";
   };
 }
