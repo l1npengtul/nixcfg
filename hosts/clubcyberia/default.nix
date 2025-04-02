4329ed7555e83d4b3a93955b88c5d7376244afc9
l1npengtul <l1npengtul@protonmail.com>
2025-04-02 22:31:31 +0900

    wadwa

---
+++ hosts/clubcyberia/default.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  imports = [
+#     ./hardware-configuration.nix
+    ./../common/userl1npengtu.nix
+  ];
+
+  time.timeZone = "Asia/Tokyo";
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+#   fileSystems."/home/l1npengtul/project" = {
+#     device = "/dev/disk/by-uuid/125413e8-0b7a-464f-9d83-fa4e00f00a35";
+#     fsType = "ext4";
+#   };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  networking.hostName = "clubcyberia";
+
+  musnix.enable = true;
+  musnix.rtcqs.enable = true;
+
+  system.stateVersion = "24.11";
+}
