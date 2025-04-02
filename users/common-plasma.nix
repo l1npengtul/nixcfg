b6fb31d4b1161800f57b305a9135e3f9a7408c43
l1npengtul <l1npengtul@protonmail.com>
2025-04-03 01:25:47 +0900

    l1npengtul@clubcyberia: update plasma/clubcyberia.nix

--- users/common-plasma.nix
+++ users/common-plasma.nix
@@ -21,46 +21,26 @@
     };
   };
 
-  programs.nushell = {
+  programs.fish = {
     enable = true;
-    # The config.nu can be anywhere you want if you like to edit your Nushell with Nu
-    configFile.source = ./config.nu;
-    # for editing directly to config.nu
-    extraConfig = ''
-      let carapace_completer = {|spans|
-      carapace $spans.0 nushell $spans | from json
-      }
-      $env.config = {
-        hooks: {
-          pre_prompt: [{ ||
-            if (which direnv | is-empty) {
-              return
-            }
-
-            direnv export json | from json | default {} | load-env
-          }]
-        }
-       show_banner: false,
-       completions: {
-       case_sensitive: false # case-sensitive completions
-       quick: true    # set to false to prevent auto-selecting completions
-       partial: true    # set to false to prevent partial filling of the prompt
-       algorithm: "fuzzy"    # prefix or fuzzy
-       external: {
-       # set to false to prevent nushell looking into $env.PATH to find more suggestions
-           enable: true
-       # set to lower can improve completion performance at the cost of omitting some options
-           max_results: 100
-           completer: $carapace_completer # check 'carapace_completer'
-         }
-       }
-      }
-      $env.PATH = ($env.PATH |
-      split row (char esep) |
-      prepend /home/myuser/.apps |
-      append /usr/bin/env
-      )
+    interactiveShellInit = ''
+      starship init fish | source
     '';
+    plugins = [
+      # Enable a plugin (here grc for colorized command output) from nixpkgs
+      {
+        name = "grc";
+        src = pkgs.fishPlugins.grc.src;
+      }
+      {
+        name = "fzf";
+        src = pkgs.fishPlugins.fzf.src;
+      }
+      {
+        name = "z";
+        src = pkgs.fishPlugins.z.src;
+      }
+    ];
   };
 
   xdg.enable = true;
