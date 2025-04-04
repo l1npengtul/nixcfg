2fbf46a2033f1e0c182e6931afbbf36109f4e7b4
l1npengtul <l1npengtul@protonmail.com>
2025-04-04 14:43:26 +0900

    l1npengtul@clubcyberia: Fri Apr  4 02:43:26 PM JST 2025 -

--- hosts/common/userl1npengtul.nix
+++ hosts/common/userl1npengtul.nix
@@ -22,6 +22,6 @@
   users.users."l1npengtul".openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHmy492dN8mCQIP/f/ecxu9DIBHbhQF5Yte28CJZ1hgf l1npengtul@protonmail.com"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBITwQ7bUgxNBYdQFzWjRQkg9NW9s646icQZ/ifVitD4 l1npengtul@protonmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6YTGD3iFT4DkdF9Qk4yE+Uxj3T2rjr3Qwh9KGLzwBZ l1npengtul@protonmail.com"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbe88oJjlFPbgN4pn4v5cdAWGEDiOiwQnEpTXPzSaYS l1npengtul@protonmail.com"
   ];
 }
