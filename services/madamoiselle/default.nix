66aad6d76e33ca0132f3ee201cf6a0e0af91b8e4
l1npengtul <l1npengtul@protonmail.com>
2025-04-07 00:23:15 +0900

    mada

---
+++ services/madamoiselle/default.nix
@@ -0,0 +1,27 @@
+{config, pkgs, lib, callPackage}: let
+   cfg = config.services.madamoiselle;
+   madamoiselle = import ./madamoiselle.nix {};
+   madamoiselle-pkg = callPackage ./madamoiselle.nix {};
+ in
+{
+    options = {
+        services.madamoiselle = {
+            enable = lib.mkEnableOption "enable madamoiselle";
+        };
+    };
+
+    config = lib.mkIf cfg.enable {
+        systemd.services.madamoiselle = {
+            wantedBy = [ "default.target" ];
+            after = ["network.target"];
+            description = "enable madamoiselle discord bot";
+            serviceConfig = {
+                StateDirectory = "foo";
+                StateDirectoryMode = "0777";
+                ExecStart = "${madamoiselle}/bin/madamoiselle";
+            };
+        };
+    };
+
+    environment.systemPackages = [madamoiselle-pkg];
+}
---
+++ services/madamoiselle/madamoiselle.nix
@@ -0,0 +1,22 @@
+{
+  rustPlatform,
+  fetchFromGitHub,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "cargo-3ds";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "l1npengtul";
+    repo = "madamoiselle";
+    tag = "${version}";
+    hash = "";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "";
+
+  # Integration tests do not run in Nix build environment due to needing to
+  # create and build Cargo workspaces.
+  doCheck = false;
+}
