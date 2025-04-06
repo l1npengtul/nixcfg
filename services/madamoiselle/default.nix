40f5445a92361436f4b0ad2637d505b78cd280a0
l1npengtul <l1npengtul@protonmail.com>
2025-04-07 00:36:12 +0900

    pengsrv@abandonedfactory: Mon Apr  7 12:36:12 AM JST 2025 -

--- services/madamoiselle/default.nix
+++ services/madamoiselle/default.nix
@@ -1,7 +1,11 @@
-{callPackage, ...}: let
+{
+  config,
+  callPackage,
+  ...
+}: let
   service = import ./service.nix {};
   package = callPackage ./madamoiselle.nix {};
 in {
   environment.systemPackages = [package];
-  config.services.madamoiselle.enable = true;
+  services.madamoiselle.enable = true;
 }
