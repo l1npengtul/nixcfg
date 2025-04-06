3342c43a4bd98010437bef3250654d3c419e6a38
l1npengtul <l1npengtul@protonmail.com>
2025-04-07 01:04:47 +0900

    pengsrv@abandonedfactory: Mon Apr  7 01:04:47 AM JST 2025 -

--- services/madamoiselle/madamoiselle.nix
+++ services/madamoiselle/madamoiselle.nix
@@ -3,16 +3,17 @@
   fetchFromGitHub,
 }:
 rustPlatform.buildRustPackage rec {
-  pname = "cargo-3ds";
-  version = "0.1.0";
+  pname = "madamoiselle";
+  version = "0.1.1";
 
   src = fetchFromGitHub {
     owner = "l1npengtul";
     repo = "madamoiselle";
     tag = "${version}";
-    hash = "sha256-EGsE/XM0/i+p9emOV7iOzI5rTLv0UGEWI7SRGS+woOU=";
+    hash = "";
   };
 
+  useFetchCargoVendor = true;
   cargoHash = "";
 
   # Integration tests do not run in Nix build environment due to needing to
