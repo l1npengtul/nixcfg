5432b383cffed574c7446bd6b2c484e8fae936ff
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 16:51:14 +0900

    TEST: l1npengtul@clubcyberia: Sun Jun  1 04:51:14 PM JST 2025 -

--- pkgs/audio/demucs/lameenc.nix
+++ pkgs/audio/demucs/lameenc.nix
@@ -18,7 +18,6 @@ buildPythonPackage rec {
     sha256 = "68dbacf39fc049dae22b08614d363d3293822d7255908b1bfe50d51c1a0fd6a1";
     dist = "py3";
     python = "py3";
-    abi = "cp313t";
     platform = "manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64";
   };
 
