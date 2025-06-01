8b5318bd8d79c0b119c8a59a817399ffb46d57a0
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 17:08:03 +0900

    TEST: l1npengtul@clubcyberia: Sun Jun  1 05:08:03 PM JST 2025 -

--- pkgs/audio/demucs/lameenc.nix
+++ pkgs/audio/demucs/lameenc.nix
@@ -2,7 +2,7 @@
   lib,
   pkgs,
   buildPythonPackage,
-  fetchPypi,
+  fetchUrl,
   # build-system
   setuptools,
   setuptools-scm,
@@ -13,12 +13,9 @@ buildPythonPackage rec {
   version = "1.8.1";
   format = "wheel";
 
-  src = fetchPypi {
-    inherit pname version format;
-    sha256 = "68dbacf39fc049dae22b08614d363d3293822d7255908b1bfe50d51c1a0fd6a1";
-    dist = "cp313";
-    python = "py3";
-    platform = "manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64";
+  src = fetchUrl {
+    url = "https://files.pythonhosted.org/packages/c6/52/f5f2d979372f3718e2792e8c3390f9eb89bc836eb484c85110c3333c9813/lameenc-1.8.1-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl";
+    hash = "sha256-68dbacf39fc049dae22b08614d363d3293822d7255908b1bfe50d51c1a0fd6a1";
   };
 
   doCheck = false;
