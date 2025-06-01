394a910382e6d988ca40f87cff7c328620997f7f
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 17:10:19 +0900

    TEST: l1npengtul@clubcyberia: Sun Jun  1 05:10:19 PM JST 2025 -

--- pkgs/audio/demucs/lameenc.nix
+++ pkgs/audio/demucs/lameenc.nix
@@ -11,7 +11,7 @@ python3Packages.buildPythonPackage rec {
 
   src = fetchurl {
     url = "https://files.pythonhosted.org/packages/c6/52/f5f2d979372f3718e2792e8c3390f9eb89bc836eb484c85110c3333c9813/lameenc-1.8.1-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl";
-    hash = "sha256-68dbacf39fc049dae22b08614d363d3293822d7255908b1bfe50d51c1a0fd6a1";
+    hash = "";
   };
 
   doCheck = false;
