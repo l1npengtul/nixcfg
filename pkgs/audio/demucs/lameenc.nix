4b51316ab8b1c0cce36832943b5e16c75305be35
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 17:09:46 +0900

    TEST: l1npengtul@clubcyberia: Sun Jun  1 05:09:46 PM JST 2025 -

--- pkgs/audio/demucs/lameenc.nix
+++ pkgs/audio/demucs/lameenc.nix
@@ -1,7 +1,7 @@
 {
   lib,
   python3Packages,
-  fetchUrl,
+  fetchurl,
   lame,
 }:
 python3Packages.buildPythonPackage rec {
@@ -9,7 +9,7 @@ python3Packages.buildPythonPackage rec {
   version = "1.8.1";
   format = "wheel";
 
-  src = fetchUrl {
+  src = fetchurl {
     url = "https://files.pythonhosted.org/packages/c6/52/f5f2d979372f3718e2792e8c3390f9eb89bc836eb484c85110c3333c9813/lameenc-1.8.1-cp313-cp313t-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl";
     hash = "sha256-68dbacf39fc049dae22b08614d363d3293822d7255908b1bfe50d51c1a0fd6a1";
   };
