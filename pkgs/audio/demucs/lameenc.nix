5406f04819aa766d88d7f062f32d6d8b7e4702a4
l1npengtul <l1npengtul@protonmail.com>
2025-06-01 18:05:39 +0900

    TEST: l1npengtul@clubcyberia: Sun Jun  1 06:05:39 PM JST 2025 -

--- pkgs/audio/demucs/lameenc.nix
+++ pkgs/audio/demucs/lameenc.nix
@@ -39,52 +39,52 @@ python3Packages.buildPythonPackage rec {
     rm setup.py
     cat > setup.py << EOF
     import sys
-      import setuptools
-      import distutils.core
-      import os.path
+    import setuptools
+    import distutils.core
+    import os.path
 
-      print(sys.argv)
-      libdir = '${lame.lib}/lib'
-      incdir = '${lame.src}/include'
+    print(sys.argv)
+    libdir = '${lame.lib}/lib'
+    incdir = '${lame.src}/include'
 
-      # Create the extension
-      lameenc = distutils.core.Extension(
-          'lameenc',
-          include_dirs=[incdir] if incdir else [],
-          libraries=['libmp3lame'] if sys.platform == 'win32' else [],
-          extra_objects=
-              [] if sys.platform == 'win32' or not libdir else [os.path.join(libdir, 'libmp3lame.a')],
-          library_dirs=[libdir] if sys.platform == 'win32' and libdir else [],
-          sources=['lameenc.c']
-      )
+    # Create the extension
+    lameenc = distutils.core.Extension(
+        'lameenc',
+        include_dirs=[incdir] if incdir else [],
+        libraries=['libmp3lame'] if sys.platform == 'win32' else [],
+        extra_objects=
+            [] if sys.platform == 'win32' or not libdir else [os.path.join(libdir, 'libmp3lame.a')],
+        library_dirs=[libdir] if sys.platform == 'win32' and libdir else [],
+        sources=['lameenc.c']
+    )
 
-      configuration = dict(
-          name='lameenc',
-          description='LAME encoding bindings',
-          long_description='''
-      Python 3 bindings for the LAME encoding library.
-      This library makes it simple to encode PCM data into MP3 without having
-      to compile any binaries.
+    configuration = dict(
+        name='lameenc',
+        description='LAME encoding bindings',
+        long_description='''
+    Python 3 bindings for the LAME encoding library.
+    This library makes it simple to encode PCM data into MP3 without having
+    to compile any binaries.
 
-      Provides binaries in PyPi for Python 3.8+ for Windows, macOS and Linux.
-      ''',
-          author='Chris Staite',
-          author_email='chris@yourdreamnet.co.uk',
-          url='https://github.com/chrisstaite/lameenc',
-          license='LGPLv3',
-          ext_modules=[lameenc],
-          classifiers=[
-              'Topic :: Multimedia :: Sound/Audio :: Conversion',
-              'Programming Language :: Python :: 3 :: Only',
-              'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-              'Operating System :: MacOS :: MacOS X',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX :: Linux'
-          ]
-      )
+    Provides binaries in PyPi for Python 3.8+ for Windows, macOS and Linux.
+    ''',
+        author='Chris Staite',
+        author_email='chris@yourdreamnet.co.uk',
+        url='https://github.com/chrisstaite/lameenc',
+        license='LGPLv3',
+        ext_modules=[lameenc],
+        classifiers=[
+            'Topic :: Multimedia :: Sound/Audio :: Conversion',
+            'Programming Language :: Python :: 3 :: Only',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: Microsoft :: Windows',
+            'Operating System :: POSIX :: Linux'
+        ]
+    )
 
-      # Create the package
-      setuptools.setup(**configuration)
-      EOF
+    # Create the package
+    setuptools.setup(**configuration)
+    EOF
   '';
 }
