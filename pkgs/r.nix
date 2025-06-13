04be480981f79db210358ddfb3d6fcfa5faf087e
l1npengtul <l1npengtul@protonmail.com>
2025-06-13 15:09:33 +0900

    l1npengtul@oldhome: Fri Jun 13 15:09:33 JST 2025 -

--- pkgs/r.nix
+++ pkgs/r.nix
@@ -1,6 +1,6 @@
-{pkgs, ...}:
-with pkgs; let
-  customR = rstudioWrapper.override {packages = with rPackages; [ggplot2 dplyr xts rmarkdown knitr BSDA MASS lme4 blme MuMIn DHARMa];};
+{pkgs, ...}: let
+  customRStudio = pkgs.rstudioWrapper.override {packages = with pkgs.rPackages; [ggplot2 dplyr xts rmarkdown knitr BSDA MASS lme4 blme MuMIn DHARMa FSA];};
+  customR = pkgs.rWrapper.override {packages = with pkgs.rPackages; [ggplot2 dplyr xts rmarkdown knitr BSDA MASS lme4 blme MuMIn DHARMa FSA];};
 in {
-  environment.systemPackages = with pkgs; [customR];
+  environment.systemPackages = [customR customRStudio];
 }
