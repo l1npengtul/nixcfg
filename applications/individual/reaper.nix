77770882362ffc14f3d829accb669aaa8b5ce76b
l1npengtul <l1npengtul@protonmail.com>
2025-06-19 03:56:46 +0900

    l1npengtul@clubcyberia: Thu Jun 19 03:56:46 AM JST 2025 -

--- applications/individual/reaper.nix
+++ applications/individual/reaper.nix
@@ -1,5 +1,5 @@
 {
-  config,
+  homedir,
   pkgs,
   ...
 }: {
@@ -9,14 +9,23 @@
     reaper-sws-extension
   ];
 
-  xdg.configFile.REAPER = {
-    enable = true;
-    source = pkgs.symlinkJoin {
-      name = "reaper-userplugins";
-      paths = with pkgs; [
-        reaper-sws-extension
-        reaper-reapack-extension
-      ];
-    };
+  home.file.sws-extension = {
+    source = "${pkgs.reaper-sws-extension}/UserPlugins/reaper_sws-x86_64.so";
+    target = "${homedir}/.config/REAPER/UserPlugins/reaper_sws-x86_64.so";
+  };
+
+  home.file.sws-script1 = {
+    source = "${pkgs.reaper-sws-extension}/Scripts/sws_python.py";
+    target = "${homedir}/.config/REAPER/Scripts/sws_python.py";
+  };
+
+  home.file.sws-script2 = {
+    source = "${pkgs.reaper-sws-extension}/Scripts/sws_python64.py";
+    target = "${homedir}/.config/REAPER/Scripts/sws_python64.py";
+  };
+
+  home.file.reapack = {
+    source = "${pkgs.reaper-reapack-extension}/UserPlugins/reaper_reapack-x86_64.so";
+    target = "${homedir}/.config/REAPER/UserPlugins/reaper_reapack-x86_64.so";
   };
 }
