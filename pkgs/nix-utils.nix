8602e05726ca4e856057321457175dd763f081ae
l1npengtul <l1npengtul@protonmail.com>
2025-06-20 15:17:39 +0900

    l1npengtul@oldhome: Fri Jun 20 15:17:39 JST 2025 -

--- pkgs/nix-utils.nix
+++ pkgs/nix-utils.nix
@@ -3,6 +3,6 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = [pkgs.nil pkgs.nixfmt-rfc-style inputs.alejandra.defaultPackage.${pkgs.system} pkgs.patchelfUnstable pkgs.file pkgs.nix-prefetch-github pkgs.nixpkgs-review pkgs.nix-update];
+  environment.systemPackages = [pkgs.nil pkgs.nixfmt-rfc-style inputs.alejandra.defaultPackage.${pkgs.system} pkgs.patchelfUnstable pkgs.file pkgs.nix-prefetch-github pkgs.nixpkgs-review pkgs.nix-update pkgs.nix-du];
   programs.nix-index-database.comma.enable = true;
 }
