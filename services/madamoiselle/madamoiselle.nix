5536ea5e5f8536e77034152980a23a4d5372d748
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 01:19:40 +0900

    pengsrv@abandonedfactory: Fri Jul  4 01:19:40 AM JST 2025 -

--- services/madamoiselle/madamoiselle.nix
+++ services/madamoiselle/madamoiselle.nix
@@ -2,24 +2,31 @@
   rustPlatform,
   fetchFromGitHub,
   sqlx-cli,
-}:
-rustPlatform.buildRustPackage rec {
-  pname = "madamoiselle";
+}: let
   version = "0.2.0";
+in
+  rustPlatform.buildRustPackage {
+    pname = "madamoiselle";
+    inherit version;
 
-  src = fetchFromGitHub {
-    owner = "l1npengtul";
-    repo = "madamoiselle";
-    tag = "${version}";
-    hash = "sha256-B6AJ7TYKvnJg65VakrqDB8CorSyU4sK2Qc7gUjwZsHQ=";
-  };
+    src = fetchFromGitHub {
+      owner = "l1npengtul";
+      repo = "madamoiselle";
+      tag = "${version}";
+      hash = "sha256-B6AJ7TYKvnJg65VakrqDB8CorSyU4sK2Qc7gUjwZsHQ=";
+    };
 
-  useFetchCargoVendor = true;
-  cargoHash = "sha256-GRKZ9Mysdl/BJtnp7YA9XyzrEyCVxPbG4gYruT9JUII=";
+    useFetchCargoVendor = true;
+    cargoHash = "sha256-GRKZ9Mysdl/BJtnp7YA9XyzrEyCVxPbG4gYruT9JUII=";
 
-  nativeBuildInputs = [sqlx-cli];
+    nativeBuildInputs = [sqlx-cli];
 
-  preCheck = ''
-    cargo sqlx prepare
-  '';
-}
+    preCheck = ''
+      cargo sqlx prepare
+    '';
+
+    configurePhase = ''
+      ls
+      cargo sqlx prepare
+    '';
+  }
