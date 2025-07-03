fc71b42c2fc02412232f73ac1d5046bf161849d8
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 03:26:53 +0900

    pengsrv@abandonedfactory: Fri Jul  4 03:26:53 AM JST 2025 -

--- services/madamoiselle/service.nix
+++ services/madamoiselle/service.nix
@@ -21,10 +21,8 @@ in {
       serviceConfig = {
         StateDirectory = "madamoiselle";
         RuntimeDirectory = "madamoiselle";
-        RuntimeDirectoryMode = "0777";
+        RuntimeDirectoryMode = "777";
         StateDirectoryMode = "777";
-        Environment = "RUST_LOG=info";
-        SyslogLevel = "info";
         ExecStart = "${madamoiselle}/bin/madamoiselle";
       };
     };
