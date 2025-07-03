b7c1ed9b68abde982a4fe0b97bd5caf24ae998a7
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 00:22:24 +0900

    l1npengtul@clubcyberia: Fri Jul  4 12:22:24 AM JST 2025 -

--- services/madamoiselle/madamoiselle.nix
+++ services/madamoiselle/madamoiselle.nix
@@ -4,17 +4,17 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "madamoiselle";
-  version = "0.1.5";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "l1npengtul";
     repo = "madamoiselle";
     tag = "${version}";
-    hash = "sha256-HOrrXlefzBCqwfIR16VT1N67+hVNU674K29NgnnpgCo=";
+    hash = "";
   };
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-9QY17n/WgT8IWC8UeKHV6oSXsFjXggKjsTvkvBFyQ6o=";
+  cargoHash = "";
 
   # Integration tests do not run in Nix build environment due to needing to
   # create and build Cargo workspaces.
