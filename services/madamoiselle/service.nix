67d9891531e8491247bb915dbed59b229746c076
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 02:12:37 +0900

    pengsrv@abandonedfactory: Fri Jul  4 02:12:37 AM JST 2025 -

--- services/madamoiselle/service.nix
+++ services/madamoiselle/service.nix
@@ -19,9 +19,7 @@ in {
       after = ["network.target"];
       description = "enable madamoiselle discord bot";
       serviceConfig = {
-        StateDirectory = "/var/lib/madamoiselle";
-        RuntimeDirectory = "/var/lib/madamoiselle";
-        WorkingDirectory = "/var/lib/madamoiselle";
+        StateDirectory = "madamoiselle";
         RuntimeDirectoryMode = "0777";
         Environment = "RUST_LOG=info";
         SyslogLevel = "info";
