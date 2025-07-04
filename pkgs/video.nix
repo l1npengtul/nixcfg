af5f05e87acdb3fe5a55f6e215a8503a403b23f3
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 21:27:53 +0900

    l1npengtul@clubcyberia: Fri Jul  4 09:27:53 PM JST 2025 -

--- pkgs/video.nix
+++ pkgs/video.nix
@@ -1,11 +1,4 @@
 {pkgs, ...}: let
-  libbluray = pkgs.libbluray.override {
-    withAACS = true;
-    withBDplus = true;
-    withJava = true;
-  };
-  libdvdcss = pkgs.libdvdcss;
-  vlc = pkgs.vlc.override {inherit libbluray libdvdcss;};
 in {
-  environment.systemPackages = with pkgs; [mpv vlc];
+  environment.systemPackages = with pkgs; [mpv vlc libdvdcss];
 }
