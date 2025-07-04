b5e5fa5b8941c83182d93be7103b4338b182a32d
l1npengtul <l1npengtul@protonmail.com>
2025-07-04 22:43:05 +0900

    l1npengtul@clubcyberia: Fri Jul  4 10:43:05 PM JST 2025 -

--- pkgs/video.nix
+++ pkgs/video.nix
@@ -1,4 +1,9 @@
 {pkgs, ...}: let
+  libbluray = pkgs.libbluray {
+    withBDplus = true;
+    withAACS = true;
+    withJava = true;
+  };
 in {
-  environment.systemPackages = with pkgs; [mpv vlc libdvdcss libaacs libbluray vobcopy makemkv handbrake];
+  environment.systemPackages = with pkgs; [mpv vlc libdvdcss libaacs vobcopy makemkv handbrake] ++ [libbluray];
 }
