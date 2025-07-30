95ea180fa04726282cf380c2c2354cb6f318dc48
l1npengtul <l1npengtul@protonmail.com>
2025-07-30 16:58:12 +0900

    l1npengtul@clubcyberia: Wed Jul 30 04:58:12 PM JST 2025 -

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -24,7 +24,7 @@
     ];
   };
 
-  environment.systemPackages = [pkgs.protonup-qt pkgs.r2modman inputs.flux.packages.${pkgs.system}.mcman];
+  environment.systemPackages = [pkgs.protonup-qt pkgs.r2modman inputs.flux.packages.${pkgs.system}.mcman pkgs.protontricks];
 
   hardware.steam-hardware.enable = true;
 }
