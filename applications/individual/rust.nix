c98794aecb933b351485119e238b3fa334e59c05
l1npengtul <l1npengtul@protonmail.com>
2025-08-14 08:22:26 +0900

    l1npengtul@oldhome: Thu Aug 14 08:22:26 JST 2025 -

--- applications/individual/rust.nix
+++ applications/individual/rust.nix
@@ -1,53 +1,51 @@
 {pkgs, ...}: {
-  home = {
-    programs.helix = {
-      enable = true;
-      settings = {
-        theme = "rose_pine_dawn";
-        editor = {
-          lsp.display-messages = true;
-          cursor-shape = {
-            normal = "block";
-            insert = "bar";
-            select = "underline";
-          };
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "rose_pine_dawn";
+      editor = {
+        lsp.display-messages = true;
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
         };
       };
-      languages.language = [
-        {
-          name = "nix";
-          auto-format = true;
-          formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-        }
-        {
-          name = "rust";
-          auto-format = true;
-        }
-      ];
     };
-    programs.wezterm = {
-      enable = true;
-      extraConfig = ''
-        -- Pull in the wezterm API
-        local wezterm = require 'wezterm'
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+      {
+        name = "rust";
+        auto-format = true;
+      }
+    ];
+  };
+  programs.wezterm = {
+    enable = true;
+    extraConfig = ''
+      -- Pull in the wezterm API
+      local wezterm = require 'wezterm'
 
-        -- This will hold the configuration.
-        local config = wezterm.config_builder()
+      -- This will hold the configuration.
+      local config = wezterm.config_builder()
 
-        -- This is where you actually apply your config choices.
+      -- This is where you actually apply your config choices.
 
-        -- For example, changing the initial geometry for new windows:
-        config.initial_cols = 120
-        config.initial_rows = 28
+      -- For example, changing the initial geometry for new windows:
+      config.initial_cols = 120
+      config.initial_rows = 28
 
-        -- or, changing the font size and color scheme.
-        config.font_size = 12
-        config.font = wezterm.font("Comic Sans Mono")
-        config.color_scheme = 'Sakura (base16)'
+      -- or, changing the font size and color scheme.
+      config.font_size = 12
+      config.font = wezterm.font("Comic Sans Mono")
+      config.color_scheme = 'Sakura (base16)'
 
-        -- Finally, return the configuration to wezterm:
-        return config
-      '';
-    };
+      -- Finally, return the configuration to wezterm:
+      return config
+    '';
   };
 }
