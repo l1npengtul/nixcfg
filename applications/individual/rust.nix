a7b6452c39681380dc116184dfd97d3ab9f5eb5d
l1npengtul <l1npengtul@protonmail.com>
2025-08-14 09:44:36 +0900

    l1npengtul@oldhome: Thu Aug 14 09:44:36 JST 2025 -

--- applications/individual/rust.nix
+++ applications/individual/rust.nix
@@ -4,12 +4,62 @@
     settings = {
       theme = "rose_pine_dawn";
       editor = {
-        lsp.display-messages = true;
+        line-number = "relative";
+        auto-save = true;
+        auto-format = true;
+        auto-completion = true;
+        text-width = 110;
+        gutters = ["diff" "diagnostics" "line-numbers" "spacer"];
+        soft-wrap.enable = true;
+        soft-wrap.max-indent-retain = 80;
+        mouse = true;
+
+        lsp = {
+          enable = true;
+          auto-signature-help = true;
+          display-messages = true;
+        };
+
+        file-picker = {
+          hidden = false;
+        };
+
+        statusline = {
+          left = ["mode" "spinner" "file-modification-indicator" "read-only-indicator"];
+          center = ["file-name"];
+          right = ["diagnostics" "register" "selections" "position" "file-encoding" "file-line-ending" "file-type"];
+          separator = "│";
+          mode.normal = "LOCKED";
+          mode.insert = "WORKING";
+          mode.select = "VISUAL SEL";
+        };
+
         cursor-shape = {
           normal = "block";
           insert = "bar";
           select = "underline";
         };
+      };
+
+      keys.normal = {
+        C-s = ":w"; # Maps Ctrl-s to the typable command :w which is an alias for :write (save file)
+        C-o = ":open ~/.config/helix/config.toml"; # Maps Ctrl-o to opening of the helix config file
+        a = "move_char_left"; # Maps the 'a' key to the move_char_left command
+        w = "move_line_up"; # Maps the 'w' key move_line_up
+        "C-S-esc" = "extend_line"; # Maps Ctrl-Shift-Escape to extend_line
+        g = {a = "code_action";}; # Maps `ga` to show possible code actions
+        "ret" = ["open_below" "normal_mode"]; # Maps the enter key to open_below then re-enter normal mode
+      };
+    };
+    language-server.rust-analyzer = {
+      command = "rust-analyzer";
+      config = {
+        inlayHints.bindingModeHints.enable = false;
+        inlayHints.closingBraceHints.minLines = 10;
+        inlayHints.closureReturnTypeHints.enable = "with_block";
+        inlayHints.discriminantHints.enable = "fieldless";
+        inlayHints.lifetimeElisionHints.enable = "skip_trivial";
+        inlayHints.typeHints.hideClosureInitialization = false;
       };
     };
     languages.language = [
@@ -21,6 +71,19 @@
       {
         name = "rust";
         auto-format = true;
+
+        roots = ["Cargo.toml" "Cargo.lock"];
+
+        auto-pairs = [
+          "'(' = ')'"
+          "'{' = '}'"
+          "'[' = ']'"
+          "'<' = '>'"
+          '''"' = '"' ''
+        ];
+
+        auto-format = true;
+        formatter.command = "cargo fmt";
       }
     ];
   };
