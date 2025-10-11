02f524b989856a8c8c0cfa6ebb888cb5a8e220b2
l1npengtul <l1npengtul@protonmail.com>
2025-10-11 22:16:21 +0900

    l1npengtul@clubcyberia: Sat Oct 11 10:16:21 PM JST 2025 -

--- pkgs/audio/socalabs/voc.nix
+++ pkgs/audio/socalabs/voc.nix
@@ -90,11 +90,11 @@ stdenv.mkDerivation {
     libxkbcommon
     libdatrie
     libepoxy
-    libsoup_2_4
+    libsoup_3
     lerc
     freetype
     curl
-    webkitgtk_4_0
+    webkitgtk_6_0
     pcre2
     util-linux
     sqlite
