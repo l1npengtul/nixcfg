443592efd1bddd568866f3a7c2eb908798704f73
l1npengtul <l1npengtul@protonmail.com>
2025-10-11 15:33:00 +0900

    l1npengtul@clubcyberia: Sat Oct 11 03:33:00 PM JST 2025 -

--- applications/individual/reaper.nix
+++ applications/individual/reaper.nix
@@ -9,23 +9,23 @@
     #    reaper-sws-extension
   ];
 
-  home.file.sws-extension = {
-    source = "${pkgs.reaper-sws-extension}/UserPlugins/reaper_sws-x86_64.so";
-    target = ".config/REAPER/UserPlugins/reaper_sws-x86_64.so";
-  };
+  #  home.file.sws-extension = {
+  #    source = "${pkgs.reaper-sws-extension}/UserPlugins/reaper_sws-x86_64.so";
+  #    target = ".config/REAPER/UserPlugins/reaper_sws-x86_64.so";
+  #  };
 
-  home.file.sws-script1 = {
-    source = "${pkgs.reaper-sws-extension}/Scripts/sws_python.py";
-    target = "$.config/REAPER/Scripts/sws_python.py";
-  };
+  #  home.file.sws-script1 = {
+  #    source = "${pkgs.reaper-sws-extension}/Scripts/sws_python.py";
+  #    target = "$.config/REAPER/Scripts/sws_python.py";
+  #  };
 
-  home.file.sws-script2 = {
-    source = "${pkgs.reaper-sws-extension}/Scripts/sws_python64.py";
-    target = ".config/REAPER/Scripts/sws_python64.py";
-  };
-
-  home.file.reapack = {
-    source = "${pkgs.reaper-reapack-extension}/UserPlugins/reaper_reapack-x86_64.so";
-    target = "$.config/REAPER/UserPlugins/reaper_reapack-x86_64.so";
-  };
+  #   home.file.sws-script2 = {
+  #     source = "${pkgs.reaper-sws-extension}/Scripts/sws_python64.py";
+  #     target = ".config/REAPER/Scripts/sws_python64.py";
+  #   };
+  #
+  #   home.file.reapack = {
+  #     source = "${pkgs.reaper-reapack-extension}/UserPlugins/reaper_reapack-x86_64.so";
+  #     target = "$.config/REAPER/UserPlugins/reaper_reapack-x86_64.so";
+  #   };
 }
