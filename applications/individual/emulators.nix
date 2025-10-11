211ef18b957a7b5b9384dc0e2b23083538dabf01
l1npengtul <l1npengtul@protonmail.com>
2025-10-11 17:42:23 +0900

    l1npengtul@clubcyberia: Sat Oct 11 05:42:23 PM JST 2025 -

--- applications/individual/emulators.nix
+++ applications/individual/emulators.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [
-      retroarch-free
-      mame
-      libretro.pcsx_rearmed
+      #retroarch-free
+      #mame
+      #libretro.pcsx_rearmed
       #duckstation
       pcsx2
       rpcs3
