be01d207210c74b44c9d1154bdcc3afd4e5376ff
l1npengtul <l1npengtul@protonmail.com>
2025-10-12 17:14:33 +0900

    l1npengtul@clubcyberia: Sun Oct 12 05:14:33 PM JST 2025 -

--- pkgs/aagl.nix
+++ pkgs/aagl.nix
@@ -1,8 +1,8 @@
 {...}: {
   programs.anime-game-launcher.enable = true; # Adds launcher and /etc/hosts rules
   programs.anime-games-launcher.enable = true;
-  programs.honkers-railway-launcher.enable = true;
-  programs.honkers-launcher.enable = true;
-  programs.wavey-launcher.enable = true;
-  programs.sleepy-launcher.enable = true;
+  # programs.honkers-railway-launcher.enable = true;
+  # programs.honkers-launcher.enable = true;
+  # programs.wavey-launcher.enable = true;
+  # programs.sleepy-launcher.enable = true;
 }
